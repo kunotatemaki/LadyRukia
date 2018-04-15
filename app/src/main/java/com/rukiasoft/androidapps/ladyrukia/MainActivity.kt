a33b2afe3086d7b6fcfe1a4d02da7f0e07a6df43
Raúl Feliz <raulfeliz@gmail.com>
2018-04-15 19:45:24 +0100

    add google places services to gradle, permission to manifest and do some initials tests with the api

--- app/src/main/java/com/rukiasoft/androidapps/ladyrukia/MainActivity.kt
+++ app/src/main/java/com/rukiasoft/androidapps/ladyrukia/MainActivity.kt
@@ -1,23 +1,77 @@
 package com.rukiasoft.androidapps.ladyrukia
 
+import android.app.Activity
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.api.GoogleApiClient
+import com.google.android.gms.location.places.Places
 
 import kotlinx.android.synthetic.main.activity_main.*
+import com.google.android.gms.location.places.ui.PlacePicker
+import android.widget.Toast
+import com.google.android.gms.location.places.Place
+import android.content.Intent
+import android.support.v4.app.FragmentActivity
+import android.util.Log
+import com.google.android.gms.common.api.ResultCallback
+import com.google.android.gms.location.places.PlaceLikelihood
+import com.google.android.gms.location.places.PlaceLikelihoodBuffer
 
-class MainActivity : AppCompatActivity() {
+
+
+
+
+
+class MainActivity : AppCompatActivity(), GoogleApiClient.OnConnectionFailedListener {
+
+    private val placePickerRequest = 1
+
+    override fun onConnectionFailed(p0: ConnectionResult) {
+
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        fab.setOnClickListener {
+            val builder = PlacePicker.IntentBuilder()
+
+            startActivityForResult(builder.build(this), placePickerRequest)
+        }
+
+        val mGoogleApiClient = GoogleApiClient.Builder(this)
+                .addApi(Places.GEO_DATA_API)
+                .addApi(Places.PLACE_DETECTION_API)
+                .enableAutoManage(this, this)
+                .build()
+
+        try {
+            val result = Places.PlaceDetectionApi
+                    .getCurrentPlace(mGoogleApiClient, null)
+            result.setResultCallback { likelyPlaces ->
+                for (placeLikelihood in likelyPlaces) {
+                    Log.i("cretinooooo", String.format("Place '%s' has likelihood: %g",
+                            placeLikelihood.place.name,
+                            placeLikelihood.likelihood))
+                }
+                likelyPlaces.release()
+            }
+        }catch (e: SecurityException){}
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        if (requestCode == placePickerRequest) {
+            if (resultCode == Activity.RESULT_OK) {
+                val place = PlacePicker.getPlace(data, this)
+                val toastMsg = String.format("Place: %s", place.name)
+                Toast.makeText(this, toastMsg, Toast.LENGTH_LONG).show()
+            }
         }
     }
 
